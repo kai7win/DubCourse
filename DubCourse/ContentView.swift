1401b201e11cff1a0d94367a049d7092bd4adf84
KaiChi Tsao <kai7app@gmail.com>
2022-08-22 09:08:38 +0800

    Initial Commit

---
+++ DubCourse/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DubCourse
+//
+//  Created by Kai Chi Tsao on 2022/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DubCourse/DubCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubCourseApp.swift
+//  DubCourse
+//
+//  Created by Kai Chi Tsao on 2022/8/22.
+//
+
+import SwiftUI
+
+@main
+struct DubCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
